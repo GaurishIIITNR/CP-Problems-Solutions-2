c169df615b465edd8ba94e279dc72277155b9e38
Atal Ojha <gaurish21102@iiitnr.edu.in>
2022-11-21 11:27:33 +0530

    Codeforces Solution

---
+++ 1761B.cpp
@@ -0,0 +1,88 @@
+// हर हर महादेव
+#define F first
+#define S second
+#define dbl double
+using namespace std;
+#define pb push_back
+#define vin(name, size)        \
+    for (i = 0; i < size; i++) \
+    cin >> name[i]
+#define vout(name)                    \
+    for (i = 0; i < name.size(); i++) \
+        cout << name[i] << " ";       \
+    cout << endl
+#define vvin(name, rowss, colmn)    \
+    for (i = 0; i < rowss; i++)     \
+    {                               \
+        for (j = 0; j < colmn, j++) \
+            cin >> name[i][j];      \
+    }
+#define vvout(name, rowss, colmn)      \
+    for (i = 0; i < rowss; i++)        \
+    {                                  \
+        for (j = 0; j < colmn, j++)    \
+            cout << name[i][j] << " "; \
+        cout << endl;                  \
+    }
+#include <bits/stdc++.h>
+#define ll long long int
+void dfs(vector<ll> adj[], ll node, vector<bool> &vis)
+{
+    vis[node] = 1;
+    for (auto x : adj[node])
+        if (!vis[x])
+            dfs(adj, x, vis);
+}
+// ll dream[];
+ll mod = 1000000007;
+#define all(x) x.begin(), x.end()
+#define mem memset(dream, -1, sizeof(dream))
+#define Pi 3.1415926535897932384626
+#define deb(v) cout << #v << " " << v << "\n"
+#define deb2(v, k) cout << #v << " " << v << "\n" \
+                        << #k << " " << k << "\n"
+void HHM()
+{
+    // mem;
+    ll i, j, k;
+    ll n;
+    cin >> n;
+    vector<ll> v(n, 0);
+    vin(v, n);
+    if (n <= 3)
+    {
+        cout << n << "\n";
+        return;
+    }
+    ll cnt = 0, cnt1 = 0;
+    for (i = 0; i < n; i += 2)
+    {
+        if (v[i] == v[0] && v[i + 1] == v[1])
+            cnt++;
+        else
+            break;
+    }
+    if (cnt * 2 == n)
+    {
+        cout << (n / 2) + 1 << "\n";
+        return;
+    }
+    // deb2(cnt, cnt1);
+    cout << n << "\n";
+
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    ll t = 1;
+    cin >> t;
+    for (ll i = 1; i <= t; i++)
+    {
+        // cout << "Case #" <<i<< ": ";
+        HHM();
+    }
+}
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
---
+++ 977D.cpp
@@ -0,0 +1,125 @@
+// हर हर महादेव
+#define F first
+#define S second
+#define dbl double
+using namespace std;
+#define pb push_back
+#define vin(name, size)        \
+    for (i = 0; i < size; i++) \
+    cin >> name[i]
+#define vout(name)                    \
+    for (i = 0; i < name.size(); i++) \
+        cout << name[i] << " ";       \
+    cout << endl
+#define vvin(name, rowss, colmn)    \
+    for (i = 0; i < rowss; i++)     \
+    {                               \
+        for (j = 0; j < colmn, j++) \
+            cin >> name[i][j];      \
+    }
+#define vvout(name, rowss, colmn)      \
+    for (i = 0; i < rowss; i++)        \
+    {                                  \
+        for (j = 0; j < colmn, j++)    \
+            cout << name[i][j] << " "; \
+        cout << endl;                  \
+    }
+#include <bits/stdc++.h>
+#define ll long long int
+void dfs(vector<ll> adj[], ll node, vector<bool> &vis)
+{
+    vis[node] = 1;
+    for (auto x : adj[node])
+        if (!vis[x])
+            dfs(adj, x, vis);
+}
+// ll dream[];
+ll mod = 1000000007;
+#define all(x) x.begin(), x.end()
+#define mem memset(dream, -1, sizeof(dream))
+#define Pi 3.1415926535897932384626
+#define deb(v) cout << #v << " " << v << "\n"
+#define deb2(v, k) cout << #v << " " << v << "\n" \
+                        << #k << " " << k << "\n"
+void HHM()
+{
+    // mem;
+    ll i, j, k;
+    ll n;
+    cin >> n;
+    vector<ll> v(n);
+    for (auto &x : v)
+        cin >> x;
+    map<ll, bool> mp;
+    mp[0] = 1;
+    vector<ll> r, r1;
+    r.pb(v[0]), r1.pb(v[0]);
+    while (1)
+    {
+        bool flag = 0;
+        for (i = 1; i < n; i++)
+        {
+            if (mp[i])
+                continue;
+            if (r[r.size() - 1] % 3 == 0 && v[i] == r[r.size() - 1] / 3)
+            {
+                r.pb(v[i]);
+                mp[i] = 1;
+                flag = 1;
+                break;
+            }
+            else if (r[r.size() - 1] * 2 == v[i])
+            {
+                r.pb(v[i]);
+                mp[i] = 1;
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag)
+            break;
+    }
+    while (1)
+    {
+        bool flag = 0;
+        for (i = 1; i < n; i++)
+        {
+            if (mp[i])
+                continue;
+            if (v[i] == r1[r1.size() - 1] * 3)
+            {
+                r1.pb(v[i]);
+                mp[i] = 1;
+                flag = 1;
+                break;
+            }
+            else if (r1[r1.size() - 1] % 2 == 0 && r1[r1.size() - 1] / 2 == v[i])
+            {
+                r1.pb(v[i]);
+                mp[i] = 1;
+                flag = 1;
+                break;
+            }
+        }
+        if (!flag)
+            break;
+    }
+    for (i = r1.size() - 1; i > 0; i--)
+        cout << r1[i] << " ";
+    vout(r);
+    return;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    ll t = 1;
+    // cin >> t;
+    for (ll i = 1; i <= t; i++)
+    {
+        // cout << "Case #" <<i<< ": ";
+        HHM();
+    }
+}
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt", "w", stdout);
